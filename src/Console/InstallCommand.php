3deb2c42bdd9a191b1c8d4986ade956259b8ff82
niduranga <nidurangajayarathna@gmail.com>
2026-04-06 17:27:46 +0530

    Fix: Add export npm_config_yes to bypass interactive prompt

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -52,7 +52,7 @@ class InstallCommand extends Command
 
         $basePath = base_path();
         $resourcesPath = escapeshellarg($basePath . '/resources');
-        $command = "cd {$resourcesPath} && npm create vite@latest {$folderName} -y -- --template {$framework}";
+        $command = "cd {$resourcesPath} && export npm_config_yes=true && npm create vite@latest {$folderName} -- --template {$framework}";
 
         if (app()->environment() !== 'testing') {
             shell_exec($command);
